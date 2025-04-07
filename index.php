050cf2502ad9649919e13dc8310f03a83e4d0219
nepalrohan <rohannepal098@gmail.com>
2025-04-07 18:28:21 +0545

    php basics

---
+++ index.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Learning PHP</title>
+</head>
+<body>
+    <?php
+
+$a = 50;
+$b = 60;
+
+echo "The sum is below<br>";
+echo $a+$b . "<br>";	
+
+echo "Array in php<br>";
+$fruits = ['applee', 'banana', 'orange', 'grape', 'kiwi'];
+echo $fruits[0];
+
+
+    ?>
+</body>
+</html>
