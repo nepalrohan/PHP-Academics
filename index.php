8660076305ca0c8e4e6d8f39dce9e27693c46f47
nepalrohan <rohannepal098@gmail.com>
2025-04-07 20:34:34 +0545

    functions in php

--- index.php
+++ index.php
@@ -1,47 +1,60 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Learning PHP</title>
 </head>
+
 <body>
     <?php
 
-define ("PI", 3.14);
-
-// $a = 50;
+    // define ("PI", 3.14);
+    
+    // $a = 50;
 // $b = 60;
-
-// echo "The sum is below<br>";
+    
+    // echo "The sum is below<br>";
 // echo $a+$b . "<br>";	
-
-// echo "Array in php<br>";
+    
+    // echo "Array in php<br>";
 // $fruits = ['applee', 'banana', 'orange', 'grape', 'kiwi'];
 // echo $fruits[0];
-
-// $books = array(
+    
+    // $books = array(
 //     "computer"=> "rakesh",
 //     "math"=>"kapil"
 // );
+    
+    // echo $books['computer'];
+    
 
-// echo $books['computer'];
+    // echo "<br>";
+    
 
-
-// echo "<br>";
-
-
-// $string = "RohanNepal";
+    // $string = "RohanNepal";
 // echo strrev($string);
+    
 
 
+    // echo PI;
+    
 
-echo PI;
 
+    //function in php
+
+    function average_number($num1, $num2){
+        $average = ($num1 + $num2)/2;
+        return $average;
+    }
+
+    echo average_number(40, 50);
+;
 
 
 
     ?>
 </body>
+
 </html>
